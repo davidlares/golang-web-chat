d01605cdaaf968166d13d6b0246f4dc96a5ac010
David Lares <david.e.lares@gmail.com>
2018-12-20 21:07:51 -0400

    Hello World Web

--- server.go
+++ server.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-  "fmt"
+  "github.com/gorilla/mux"
+  "net/http"
   "log"
 )
 
+func Hello(w http.ResponseWriter, r *http.Request){
+  w.Write([]byte ("Hello, World"))
+}
+
 func main(){
-  fmt.Println("Hello, World")
+  // mux URL Router
+  mux := mux.NewRouter()
+  // HTTP Route
+  mux.HandleFunc("/hello", Hello).Methods("GET")
+  // we tell http module to use MUX Routes
+  http.Handle("/", mux)
+  // message
+  log.Println("Server running on port 8000")
+  // starting server
+  log.Fatal(http.ListenAndServe(":8000", nil))
 }
