91c51e2cb89ca953b7341a47d24f336fc1f9f977
David Lares <david.e.lares@gmail.com>
2018-12-20 21:18:00 -0400

    Serving JSON

--- server.go
+++ server.go
@@ -4,10 +4,28 @@ import (
   "github.com/gorilla/mux"
   "net/http"
   "log"
+  "encoding/json"
 )
+
+// JSON Format
+type Response struct {
+  Message string `json:"message"`
+  Status bool `json:"status"`
+}
 
 func Hello(w http.ResponseWriter, r *http.Request){
   w.Write([]byte ("Hello, World"))
+}
+
+func HelloJson(w http.ResponseWriter, r *http.Request){
+  // create a Response Struct
+  response := CreateResponse()
+  // Encoding response
+  json.NewEncoder(w).Encode(response)
+}
+
+func CreateResponse() Response {
+  return Response{"This is json format", true}
 }
 
 func main(){
@@ -15,6 +33,7 @@ func main(){
   mux := mux.NewRouter()
   // HTTP Route
   mux.HandleFunc("/hello", Hello).Methods("GET")
+  mux.HandleFunc("/hello-json", HelloJson).Methods("GET")
   // we tell http module to use MUX Routes
   http.Handle("/", mux)
   // message
